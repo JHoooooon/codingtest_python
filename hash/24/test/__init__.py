7d205dd75b530a968f9c4e97f994715c428d397a
jhoon <jaehoon0822@gmail.com>
2024-03-25 08:32:45 +0900

    24 문제

---
+++ hash/24/test/__init__.py
@@ -0,0 +1,17 @@
+INPUT = [
+    [
+        ["muzi", "frodo", "apeach", "neo"],
+        ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],
+        2
+    ],
+    [
+        ["con", "ryan"],
+        ["ryan con", "ryan con", "ryan con", "ryan con"],
+        3
+    ],
+];
+
+OUTPUT = [
+    [2, 1, 1, 0],
+    [0, 0],
+]
